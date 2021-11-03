be83ca09cd21031510b6e6f3884a82b03a9cd21a
Joshua Ashley <47162599+jashley2017@users.noreply.github.com>
2021-11-03 16:50:13 -0400

    imet format fix

--- src/iMET/iMET_ros2_proto.py
+++ src/iMET/iMET_ros2_proto.py
@@ -3,7 +3,6 @@ import os
 import time
 import serial
 import sys
-from gpiozero import LED, Button
 
 import rclpy
 from rclpy.node import Node
@@ -14,40 +13,42 @@ from environ_msgs.msg import iMET_msg
 class iMET(Node):
 	def __init__(self):
 		super().__init__('iMET')
-		self.iMET_pub = self.create_publisher(IMET_msg, 'iMET_data', 10)
+		self.declare_parameter('iMET_topic', 'iMET_data')
+		self.iMET_pub = self.create_publisher(IMET_msg, self.get_parameter('iMET_topic').value, 10)
 		self.declare_parameter('iMET_port', '/dev/iMET1')
 		self.declare_parameter('iMET_baud', '9600')
 		#TODO: double check baud, may need to add parity and stop bits arguments
-		iMET1 = serial.Serial(self.get_parameter('iMET_port'), self.get_parameter('iMET_baud'))
+		iMET1 = serial.Serial(self.get_parameter('iMET_port').value, self.get_parameter('iMET_baud').value)
 		self.read_thread = threading.Thread(target=self.log_Imet, args=(iMET1,)) #TODO
 		self.running = True
-		self.read_thread.start
+		self.read_thread.start()
 	
 	def __del__(self):
 		self.running = False
 		self.read_thread.join()
 
 	def log_iMET(self, usb_device):
-		while self.running():
+		while rclpy.ok() and self.running:
 			try:
 				iMET_raw = usb_dev.readline().decode("ascii")
-				sample_time = time.time()
-
-				parsed_iMET = pynmea2.parse(iMET_raw)
+				parsed = ','.split(iMET_raw)
+				sample_time = datetime.strptime(parsed[5] + ',' + parsed[6], '%Y/%m/%d,%H:%M:%S').timestamp()
 				ros_iMET_msg = IMET_msg()
+
+				if iMET_attrs[0] == 'XQ':
+					ros_iMET_msg.pressure = parsed[1]
+					ros_iMET_msg.temp = parsed[2]
+					ros_iMET_msg.rel_humidity = parsed[3]
+					ros_iMET_msg.humidity_temp = parsed[4]
+					ros_iMET_msg.longitude = parsed[7]
+					ros_iMET_msg.latitude = parsed[8]
+					ros_iMET_msg.altitude = parsed[9]
+					ros_iMET_msg.num_sat = parsed[10]
+				else:
+					self.get_logger().warn("bad IMET message")
+				
 				ros_iMET_msg.header.stamp.sec = int(sample_time)
 				ros_iMET_msg.header.stamp.nsec = int((sample_time - int(sample_time))*1000000000)
-
-				ros_iMET_msg.XQ = parsed_iMET.XQ
-				ros_iMET_msg.pressure = parsed_iMET.pressure
-				ros_iMET_msg.temp = parsed_iMET.temp
-				ros_iMET_msg.rel_humidity = parsed_iMET.rel_humidity
-				ros_iMET_msg.humidity_temp = parsed_iMET.humidity_temp
-				ros_iMET_msg.date = parsed_iMET.date
-				ros_iMET_msg.longitude = parsed_iMET.longitude
-				ros_iMET_msg.latitude = parsed_iMET.latitude
-				ros_iMET_msg.altitude = parsed_iMET.altitude
-				ros_iMET_msg.num_sat = parsed_iMET.num_sat
 
 				self.iMET_pub(ros_iMET_msg)
 
